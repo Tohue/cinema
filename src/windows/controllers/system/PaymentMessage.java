cdc84eda523fb6fcb0b921d3a2a5b991dcaaf3cc
ekiryuhin <cheypnow@gmail.com>
2018-03-16 23:57:05 +0300

    Окно невозврата

--- src/windows/controllers/system/PaymentMessage.java
+++ src/windows/controllers/system/PaymentMessage.java
@@ -1,5 +1,8 @@
 package windows.controllers.system;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import windows.components.FontLoader;
 import windows.controllers.AbstractController;
 import windows.windowStarters.ScreenStarter;
 
@@ -7,9 +10,26 @@ import java.io.IOException;
 
 public class PaymentMessage extends AbstractController {
 
+    @FXML
+    Label title1;
+    @FXML
+    Label title2;
+    @FXML
+    Label title3;
+
+
     public  void initialize() {
 
+        setFonts();
 
+    }
+
+    private void setFonts() {
+
+        FontLoader fontLoader = new FontLoader();
+        title1.setFont(fontLoader.getBebasReg(40));
+        title2.setFont(fontLoader.getBebasReg(40));
+        title3.setFont(fontLoader.getBebasThin(40));
 
     }
 
